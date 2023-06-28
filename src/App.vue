7e68b32fad068202cc9abdb24fc317769c0156f7
BSMiguell <bruno.s.miiguel@gmail.com>
2023-06-28 20:55:16 -0300

    Fist Commit

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="app" :class="mode">
+    <HeaderView :mode="mode" @toggle="toggle" />
+    <ContentView :mode="mode" />
+  </div>
+</template>
+
+<script>
+import HeaderView from "@/components/HeaderView";
+import ContentView from "@/components/ContentView";
+
+export default {
+  name: "App",
+  data() {
+    return {
+      mode: "light",
+    };
+  },
+  components: {
+    HeaderView,
+    ContentView,
+  },
+  created() {
+    window.addEventListener("keyup", this.keyPress);
+  },
+  methods: {
+    keyPress(e) {
+      if (e.key === "t") {
+        this.toggle();
+      }
+    },
+    toggle() {
+      if (this.mode === "dark") {
+        this.mode = "light";
+      } else {
+        this.mode = "dark";
+      }
+    },
+  },
+};
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Roboto", sans-serif;
+}
+
+.app {
+  width: 100vw;
+  min-height: 100vh;
+  background: #f3f3f3;
+  color: #15202b;
+  transition: background 0.3s ease-in-out;
+}
+
+.dark {
+  background: #192734;
+  color: #f3f3f3;
+}
+</style>
---
+++ src/components/ContentView.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="content">
+    <h3>This is {{ mode }} mode</h3>
+    <div class="image-grid">
+      <div class="col">
+        <img
+          src="https://images.unsplash.com/photo-1560617137-b2bbe0e08397?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1200&h=600&fit=crop&ixid=eyJhcHBfaWQiOj F9"
+        />
+      </div>
+      <div class="col">
+        <img
+          src="https://images.unsplash.com/photo-1561327712-2e0c75a1a6e6?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1200&h=600&fit=crop&ixid=eyJhcHBfaWQiOjF9"
+        />
+      </div>
+      <div class="col">
+        <img
+          src="https://images.unsplash.com/photo-1560945928-a97ecc4dcfc7?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1200&h=600&fit=crop&ixid=eyJhcHBfaWQi0jF9"
+        />
+      </div>
+    </div>
+
+    <p class="para">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
+      corrupti quod architecto neque ipsa consectetur nisi, odio, numquam optio
+      dolore tenetur saepe dignissimos earum facilis adipisci animi quia
+      doloribus. Dolor!
+    </p>
+  </div>
+</template>
+
+<script>
+export default { props: ["mode"] };
+</script>
+
+<style>
+.content {
+  padding: 25px;
+}
+.content h3 {
+  font-size: 28px;
+  text-transform: uppercase;
+  font-weight: 300;
+  margin-bottom: 25px;
+}
+.image-grid {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 15px;
+}
+.image-grid .col img {
+  width: 100%;
+}
+.para {
+  width: 100%;
+  max-width: 600px;
+  margin: 25px auto 0px;
+}
+</style>
